18469a65147c03caa1dda2ebb75430158f46c289
pedrojesus44 <pedro.jesus44@etec.sp.gov.br>
2022-11-07 21:15:33 -0300

    consulta

--- controller/ControllerCadastro.php
+++ controller/ControllerCadastro.php
@@ -12,9 +12,7 @@ class cadastroController
         $this->cadastro = new Cadastro();
         if (isset($_GET['funcao']) && $_GET['funcao'] == "cadastro") {
             $this->incluir();
-        } else if(isset($_GET['funcao']) && $_GET['funcao'] == "editar"){
-            $this->editar($_GET['id']);
-        }
+        } 
     }
 
     private function incluir()
@@ -38,24 +36,7 @@ class cadastroController
     {
         return $result = $this->cadastro->listar($id);
     }
-
-    private function editar($id){
-        $this->cadastro->setId($id);
-        $this->cadastro->setEmail($_POST['txtEmail']);
-        $this->cadastro->setSenha($_POST['txtSenha']);
-        $this->cadastro->setEndereco($_POST['txtEndereco']);
-        $this->cadastro->setBairro($_POST['txtBairro']);
-        $this->cadastro->setCep($_POST['txtCep']);
-        $this->cadastro->setCidade($_POST['txtCidade']);
-        $this->cadastro->setEstado($_POST['txtEstado']);
-        $result = $this->cadastro->editar();
-        if($result >= 1) {
-            echo "<script>alert('Registro alterado com sucesso!');document.location='../consulta.php'</script>";
-        } else {
-            echo "<script>alert('Erro ao alterar o registro!');<script>";
-        }
-    }
-
+   
 
 }
 new cadastroController();
